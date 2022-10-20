2e4ea016f8b6ac5b46e9d7895293207983b63f35
Alex <alex@alexaa.dev>
2022-10-20 19:53:33 -0300

    fix: organizes code

--- src/components/account-overview.tsx
+++ src/components/account-overview.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import AccountOverviewHeader from './header/account-overview-header';
 
 export type Account = {
 	supportContact: {
@@ -19,31 +19,10 @@ export type Account = {
 export const AccountOverview: React.FC<{ account: Account }> = ({
 	account,
 }) => {
-	const Meta = styled.div`
-		background-color: lightblue;
-	`;
-
 	return (
-		<Meta>
-			<div>
-				<div>
-					<h1>Account Overview</h1>
-				</div>
-				<div>
-					<div>
-						<span>Your Feefo Support Contact</span>
-					</div>
-					<div>
-						<span>{account.supportContact.name.charAt(0)}</span>
-						<div>
-							<span>{account.supportContact.name}</span>
-							<span>{account.supportContact.email}</span>
-						</div>
-						<span>{account.supportContact.phone}</span>
-					</div>
-				</div>
-			</div>
-		</Meta>
+		<div>
+			<AccountOverviewHeader account={account} />
+		</div>
 	);
 };
 
--- src/components/header/account-overview-header.tsx
+++ src/components/header/account-overview-header.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Account } from '../account-overview';
-import AccountOverviewHeader from './account-overview-header';
 
-export const AccountOverview: React.FC<{ account: Account }> = ({
+export const AccountOverviewHeader: React.FC<{ account: Account }> = ({
 	account,
 }) => {
 	return (
 		<div>
-			<AccountOverviewHeader account={account} />
+			<div>
+				<h1>Account Overview</h1>
+			</div>
+			<div>
+				<div>
+					<span>Your Feefo Support Contact</span>
+				</div>
+				<div>
+					<span>{account.supportContact.name.charAt(0)}</span>
+					<div>
+						<span>{account.supportContact.name}</span>
+						<span>{account.supportContact.email}</span>
+					</div>
+					<span>{account.supportContact.phone}</span>
+				</div>
+			</div>
 		</div>
 	);
 };
 
-export default AccountOverview;
+export default AccountOverviewHeader;
